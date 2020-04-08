af44f0941ab9c6820a2c3d4d8e4ffbf440850fb4
Daria <daria.soczewka@gmail.com>
2020-04-08 23:59:21 +0200

    Corrected Edit Product window

--- managerapp-frontend/src/app/products/add-product/add-product.component.ts
+++ managerapp-frontend/src/app/products/add-product/add-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl } from '@angular/forms';
+
 import { Product } from '../../model/product';
 import { Brand } from '../../model/brand';
 import { BrandService } from '../../service/brand.service';
@@ -13,22 +15,35 @@ import { ProductService } from '../../service/product.service';
 export class AddProductComponent implements OnInit {
 
   brands: Brand[] = [];
-  newProduct: Product;
+  product: Product;
+  productForm: FormGroup;
 
   constructor(public dialogRef: MatDialogRef<AddProductComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Product,
     public brandService: BrandService,
     public productService: ProductService) {
-    this.newProduct = data;
-    console.log(this.newProduct.name);
+    this.product = data;
   }
 
   ngOnInit() {
     this.brandService.findAllBrands().subscribe(data => { this.brands = data })
+    this.productForm = new FormGroup({
+      productName: new FormControl(this.product.name),
+      selectedBrand: new FormControl(this.product.brand),
+      productInci: new FormControl(this.product.inci)
+    });
   }
 
   saveNewProduct(): void {
-    this.productService.saveNewProductInDB(this.newProduct).subscribe();
+    this.product.name = this.productForm.value.productName;
+    this.product.brand = this.productForm.value.selectedBrand;
+    this.product.inci = this.productForm.value.productInci;
+
+    this.productService.saveNewProductInDB(this.product).subscribe();
     this.dialogRef.close();
+  }
+
+  compareBrands(b1: Brand, b2: Brand): boolean {
+    return b1 && b2 ? b1.brandId === b2.brandId : b1 === b2;
   }
 }
--- managerapp-frontend/src/app/products/products.component.ts
+++ managerapp-frontend/src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../model/product';
 import { ProductsServiceService } from './products-service.service';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
 import { AddProductComponent } from './add-product/add-product.component';
 import { ProductService } from '../service/product.service';
 
